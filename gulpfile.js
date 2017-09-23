42b4c2ffcb160710b47f917258e2ae169c020ecc
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-09-23 20:13:25 +0200

    Add gulp sourcemaps and upgrade gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const babel = require('gulp-babel')
 const livereload = require('gulp-livereload')
 const rollup = require('gulp-rollup')
+const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 const pump = require('pump')
 
@@ -13,9 +14,11 @@ gulp.task('watch', () => {
 gulp.task('build', (callback) => {
   pump([
     gulp.src('src/**/*.js'),
-    rollup({entry: 'src/script.js', format: 'es'}),
+    sourcemaps.init(),
+    rollup({input: 'src/script.js', format: 'es'}),
     babel({presets: ['es2015']}),
     uglify({toplevel: true}),
+    sourcemaps.write('.'),
     gulp.dest('dist'),
     livereload()
   ], callback)
