b54e11658b07eed495ae47edee5d5db5a61e024a
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-10-02 09:28:55 +0200

    More colors (as requested by Lara)

--- projects/busy-intersections/settings.js
+++ projects/busy-intersections/settings.js
@@ -1,7 +1,26 @@
 export default {
   // Colors chosen from material.io color palettes
   // https://material.io/guidelines/style/color.html#color-color-tool
-  colors: ['#F44336', '#FF9800', '#03A9F4', '#8BC34A', '#673AB7'],
+  colors: [
+    '#F44336', // Red
+    '#E91E63', // Pink
+    '#9C27B0', // Purple
+    '#673AB7', // Deep Purple
+    '#3F51B5', // Indigo
+    '#2196F3', // Blue
+    '#03A9F4', // Light Blue
+    '#00BCD4', // Cyan
+    '#009688', // Teal
+    '#4CAF50', // Green
+    '#8BC34A', // Light Green
+    '#CDDC39', // Lime
+    '#FFEB3B', // Yellow
+    '#FFC107', // Amber
+    '#FF9800', // Orange
+    '#FF5722', // Deep Orange
+    '#795548', // Brown
+    '#607D8B' // Blue Grey
+  ],
 
   // Minimum and maximum velocity of cars in a lane
   velocity: {
